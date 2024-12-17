2a0135c44345671f9a77cbc62d48d486125e2364
Rajit jain <noddysinghai053@gmail.com>
2024-12-17 23:41:02 +0530

    tic_toc_toe game completed

---
+++ tic_tac_toe/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Tictactoe from './components/Tictactoe'
+
+function App() {
+
+  return (
+    <>
+     {/* <h1 className="text-3xl font-bold text-center underline h1tag">
+      Hello world!
+    </h1> */}
+    <Tictactoe />
+    </>
+  )
+}
+
+export default App
