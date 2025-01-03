702c86925b2a86fdae37551d20196557dba0d4e9
Rajit jain <noddysinghai053@gmail.com>
2025-01-03 14:26:55 +0530

    done styling

--- React_form/src/App.jsx
+++ React_form/src/App.jsx
@@ -21,10 +21,10 @@ function App() {
   }
   return (
    <div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-      <label htmlFor="">Name : </label>
-      <input type="text"   {...register("name", 
+    <form className='form' onSubmit={handleSubmit(onSubmit)}>
+      <div className="divField">
+      <label className="labels" htmlFor="">Name : </label>
+      <input className="inputfields"type="text"   {...register("name", 
       {
         required: true,
         minLength:{value:3 , message: "Min length should be greater than 3"}
@@ -33,31 +33,31 @@ function App() {
       </div>
       {errors.name && <p>{errors.name.message}</p>}
 
-      <div>
-      <label htmlFor="">Password : </label>
-      <input type="password"  {...register("password", {
+      <div className="divField">
+      <label className="labels" htmlFor="">Password : </label>
+      <input className="inputfields"type="password"  {...register("password", {
         required: true,
         pattern: {value : /^[A-Za-z]+$/i , message: "should include any charcter"}
       })} />
       </div>
       {errors.password && <p>{errors.password.message}</p>}
 
-      <div>
-      <label htmlFor="">Age : </label>
-      <input type="number" {...register("age", {
+      <div className="divField">
+      <label className="labels" htmlFor="">Age : </label>
+      <input className="inputfields"type="number" {...register("age", {
         required: true
       })} />
       </div>
 
-      <div>
-      <label htmlFor="">sex : </label>
-      <input type="text" {...register("sex", {
+      <div className="divField">
+      <label className="labels" htmlFor="">sex : </label>
+      <input className="inputfields"type="text" {...register("sex", {
         required: true
       })} />
       </div>
 
     
-      <input type="submit" disabled={isSubmitting}
+      <input className="inputfields Submit"type="submit" disabled={isSubmitting}
       value={isSubmitting ? "Submitting" : "submit"}/>
     </form>
 
